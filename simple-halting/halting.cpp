a06142660adc400e8884203acd703927b9d398ff
Gustavo Zambonin <gzmbnn@gmail.com>
2018-10-07 00:20:43 -0300

    halting: most tests passing * program that calculates fibonacci numbers with two recursions is failing

--- simple-halting/halting.cpp
+++ simple-halting/halting.cpp
@@ -10,9 +10,23 @@
 
 static const uint16_t constexpr MAX_INT = 1000;
 
+using state = std::pair<uint32_t, std::unordered_map<std::string, uint16_t>>;
+
+struct pair_hash {
+  std::size_t operator()(const state &p) const {
+    uint32_t summation = 0;
+    for (const auto &reg : p.second) {
+      summation += reg.second;
+    }
+
+    return std::hash<uint32_t>{}(p.first) ^ std::hash<uint32_t>{}(summation);
+  }
+};
+
 struct program {
   std::vector<std::string> lines;
   std::unordered_map<uint32_t, uint32_t> cond_markers;
+  std::unordered_map<state, bool, pair_hash> recursion_states;
 
   std::unordered_map<std::string, uint16_t> reg_bank = {
       {"R0", -1}, {"R1", 0}, {"R2", 0}, {"R3", 0}, {"R4", 0},
@@ -70,6 +84,108 @@ program load_program(std::istream &file, const std::regex &match) {
   return p;
 }
 
+uint16_t parse_arg(const program &p, const std::string &s) {
+  if (s.empty()) {
+    return -1;
+  }
+  try {
+    return p.reg_bank.at(s);
+  } catch (const std::out_of_range &e) {
+    return std::stoi(s);
+  }
+}
+
+uint16_t evaluate(program &p) {
+  uint32_t current = 0;
+  std::stack<uint32_t> calls, inputs;
+  while (current != p.lines.size()) {
+    std::stringstream ss(p.lines[current]);
+    std::string instruction, args, arg1, arg2;
+    std::getline(ss, instruction, ' ');
+    std::getline(ss, args, ' ');
+    std::stringstream sss(args);
+    std::getline(sss, arg1, ',');
+    std::getline(sss, arg2, ',');
+    auto inst =
+        std::make_tuple(instruction, parse_arg(p, arg1), parse_arg(p, arg2));
+
+    if (instruction == "IFEQ") {
+      current = (std::get<1>(inst) == std::get<2>(inst))
+                    ? current
+                    : p.cond_markers[current];
+    } else if (instruction == "IFNEQ") {
+      current = (std::get<1>(inst) != std::get<2>(inst))
+                    ? current
+                    : p.cond_markers[current];
+    } else if (instruction == "IFG") {
+      current = (std::get<1>(inst) > std::get<2>(inst))
+                    ? current
+                    : p.cond_markers[current];
+    } else if (instruction == "IFGE") {
+      current = (std::get<1>(inst) >= std::get<2>(inst))
+                    ? current
+                    : p.cond_markers[current];
+    } else if (instruction == "IFL") {
+      current = (std::get<1>(inst) < std::get<2>(inst))
+                    ? current
+                    : p.cond_markers[current];
+    } else if (instruction == "IFLE") {
+      current = (std::get<1>(inst) <= std::get<2>(inst))
+                    ? current
+                    : p.cond_markers[current];
+    } else if (instruction == "MOV") {
+      p.reg_bank[arg1] = std::get<2>(inst);
+    } else if (instruction == "ADD") {
+      p.reg_bank[arg1] = (p.reg_bank[arg1] + std::get<2>(inst)) % MAX_INT;
+    } else if (instruction == "SUB") {
+      p.reg_bank[arg1] = (p.reg_bank[arg1] - std::get<2>(inst)) % MAX_INT;
+      if (p.reg_bank[arg1] == 65535) {
+        p.reg_bank[arg1] = MAX_INT - 1;
+      }
+    } else if (instruction == "MUL") {
+      p.reg_bank[arg1] = (p.reg_bank[arg1] * std::get<2>(inst)) % MAX_INT;
+    } else if (instruction == "DIV") {
+      if (std::get<2>(inst) == 0) {
+        p.reg_bank[arg1] = 0;
+      } else {
+        p.reg_bank[arg1] = (p.reg_bank[arg1] / std::get<2>(inst)) % MAX_INT;
+      }
+    } else if (instruction == "MOD") {
+      if (std::get<2>(inst) == 0) {
+        p.reg_bank[arg1] = 0;
+      } else {
+        p.reg_bank[arg1] = (p.reg_bank[arg1] % std::get<2>(inst)) % MAX_INT;
+      }
+    } else if (instruction == "CALL") {
+      calls.push(current);
+      inputs.push(p.reg_bank["R0"]);
+
+      current = -1;
+      p.reg_bank["R0"] = std::get<1>(inst);
+      auto s = std::make_pair(current, p.reg_bank);
+      if (p.recursion_states.find(s) == p.recursion_states.end()) {
+        p.recursion_states.insert(std::make_pair(s, true));
+      } else {
+        return MAX_INT;
+      }
+    } else if (instruction == "RET") {
+      p.reg_bank["R9"] = std::get<1>(inst);
+      if (static_cast<unsigned int>(!calls.empty()) != 0u) {
+        current = calls.top();
+        calls.pop();
+        p.reg_bank["R0"] = inputs.top();
+        inputs.pop();
+      } else {
+        return p.reg_bank["R9"];
+      }
+    }
+
+    current++;
+  }
+
+  return p.reg_bank["R9"];
+}
+
 int32_t main() {
   const std::string integers("([1-9]?[0-9]{1,2})|1000"), registers("R[0-9]"),
       operand("(" + integers + "|" + registers + ")"), no_arg_key("ENDIF"),
@@ -85,6 +201,12 @@ int32_t main() {
   program p{};
   do {
     p = load_program(std::cin, valid_lines);
+    uint16_t result = evaluate(p);
+    if (result == MAX_INT) {
+      std::cout << "*" << std::endl;
+    } else if (static_cast<uint8_t>(!p.lines.empty()) != 0u) {
+      std::cout << result << std::endl;
+    }
   } while (static_cast<uint8_t>(!p.lines.empty()) != 0u);
 
   exit(EXIT_SUCCESS);
