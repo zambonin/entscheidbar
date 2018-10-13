f0438a615cb9133eac9140ccf1e74f7bf16aaafb
Gustavo Zambonin <gzmbnn@gmail.com>
2018-10-13 00:10:53 -0300

    cyk: parsing words correctly * TODO: lots of optimizations

--- cyk/cyk.cpp
+++ cyk/cyk.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <regex>
+#include <set>
 #include <string_view>
 #include <unordered_map>
 #include <unordered_set>
@@ -52,11 +53,80 @@ grammar load_grammar(std::istream &in) {
   return g;
 }
 
+std::set<char> get_rules_for_symbols(const grammar &g,
+                                     const std::string &symb) {
+  std::set<char> result;
+  for (const auto &rule : g.rules) {
+    for (const auto &poss : rule.second) {
+      bool term = symb.size() == 1 && poss[0] == symb[0];
+      bool non_term = symb.size() == 2 && poss == symb;
+      if (term || non_term) {
+        result.insert(rule.first);
+      }
+    }
+  }
+  return result;
+}
+
+std::set<char> get_rules_for_symbol(const grammar &g, const char &c) {
+  return get_rules_for_symbols(g, std::string({c}));
+}
+
+std::set<std::string> cartesian_prod(const std::set<char> &a,
+                                     const std::set<char> &b) {
+  std::set<std::string> result;
+  for (const char &a1 : a) {
+    for (const char &b1 : b) {
+      result.insert(std::string({a1, b1}));
+    }
+  }
+  return result;
+}
+
+bool cyk(const grammar &g, const std::string &w) {
+  const std::string::size_type n = w.size();
+  std::vector<std::vector<std::set<char>>> table(
+      n, std::vector<std::set<char>>(n));
+
+  for (uint32_t i = 0; i < n; ++i) {
+    std::set<char> rules = get_rules_for_symbol(g, w[i]);
+    table[i][i].insert(rules.begin(), rules.end());
+  }
+
+  for (uint32_t l = 1; l < n; l++) {
+    for (uint32_t r = 0; r < n - l; r++) {
+      for (uint32_t t = 0; t < l; t++) {
+        std::set<std::string> prod =
+            cartesian_prod(table[r][r + t], table[r + t + 1][r + l]);
+        for (const auto &pair : prod) {
+          std::set<char> rules = get_rules_for_symbols(g, pair);
+          table[r][r + l].insert(rules.begin(), rules.end());
+        }
+      }
+    }
+  }
+
+  return table[0][n - 1].find(g.start_prod) != table[0][n - 1].end();
+}
+
+void cyk_wrapper(const grammar &g, const uint32_t index) {
+  std::cout << "Instancia " << index << std::endl;
+  for (const std::string &word : g.possible_inputs) {
+    std::string result = cyk(g, word) ? "" : " nao";
+    std::cout << word << result << " e uma palavra valida" << std::endl;
+  }
+  std::cout << std::endl;
+}
+
 int32_t main() {
   grammar g{};
+  uint32_t i = 0;
 
   do {
     g = load_grammar(std::cin);
+    if (g.start_prod != 0) {
+      cyk_wrapper(g, ++i);
+    }
   } while (g.start_prod != 0);
 
   exit(EXIT_SUCCESS);
